090e2aecdee98c5c2247cce0945e918a02d68ba3
MESUT KAYA <mesutkaya2000@gmail.com>
2021-12-20 16:55:06 +0300

    JsonToList

--- Coin-Market-Example/Controllers/HomeController.cs
+++ Coin-Market-Example/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -15,23 +16,44 @@ namespace Coin_Market_Example.Controllers
             makeAPICall(); 
             return View();
         }
-        static string makeAPICall()
+        public string makeAPICall()
         {
-            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
+            string URL = ("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
 
             var queryString = HttpUtility.ParseQueryString(string.Empty);
             queryString["start"] = "1";
             queryString["limit"] = "5000";
             queryString["convert"] = "USD";
 
-            URL.Query = queryString.ToString();
+            //URL.Query = queryString.ToString();
 
             var client = new System.Net.WebClient();
             client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
             client.Headers.Add("Accepts", "application/json");
             dynamic test = client.DownloadString(URL.ToString());
+
+            var json = client.DownloadString(URL);
+            dynamic parsed = JsonConvert.DeserializeObject(json);
+            JsonToList(parsed);
             return test;
 
+        }
+
+        private void JsonToList(dynamic obj)
+        {
+            foreach (var result in obj)
+            {
+                foreach (var item in result)
+                {
+                    //ExchangeRateModel rateType = new ExchangeRateModel();
+                    //rateType.RateTypeName = result.Name;
+                    //rateType.Names = item.isim;
+                    //rateType.Buying = item.alis;
+                    //rateType.Selling = item.satis;
+                    //rateType.Changing = (Convert.ToDecimal(item.degisim) / 100);
+                    //rateTypeList.Add(rateType);
+                }
+            }
         }
     }
 }
