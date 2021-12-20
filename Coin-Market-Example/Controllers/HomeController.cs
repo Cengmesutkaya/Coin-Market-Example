e28a9b4ee7d5bf4e0a425a60f51706fbb18a5e07
MESUT KAYA <mesutkaya2000@gmail.com>
2021-12-20 21:45:15 +0300

    Changes [20.12.2021]

--- Coin-Market-Example/Controllers/HomeController.cs
+++ Coin-Market-Example/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json;
+﻿using Coin_Market_Example.Models;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,49 +11,44 @@ namespace Coin_Market_Example.Controllers
     public class HomeController : Controller
     {
         private static string API_KEY = "d785d853-d216-48bd-9f9a-c15491918b4d";
-
+        List<CoinModel> coinModelLst = new List<CoinModel>();
         public ActionResult Index()
         {
-            makeAPICall(); 
-            return View();
-        }
-        public string makeAPICall()
-        {
             string URL = ("https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest");
-
             var queryString = HttpUtility.ParseQueryString(string.Empty);
             queryString["start"] = "1";
             queryString["limit"] = "5000";
             queryString["convert"] = "USD";
 
-            //URL.Query = queryString.ToString();
-
             var client = new System.Net.WebClient();
             client.Headers.Add("X-CMC_PRO_API_KEY", API_KEY);
             client.Headers.Add("Accepts", "application/json");
-            dynamic test = client.DownloadString(URL.ToString());
 
             var json = client.DownloadString(URL);
             dynamic parsed = JsonConvert.DeserializeObject(json);
-            JsonToList(parsed);
-            return test;
-
+            if (parsed.status.error_message == null)
+            {
+                JsonToList(parsed.data);
+            }
+            else
+            {
+                throw new Exception(parsed.status.error_message);
+            }
+             return View(coinModelLst);                  
         }
 
-        private void JsonToList(dynamic obj)
+        private void JsonToList(dynamic data)
         {
-            foreach (var result in obj)
+            foreach (var result in data)
             {
-                foreach (var item in result)
-                {
-                    //ExchangeRateModel rateType = new ExchangeRateModel();
-                    //rateType.RateTypeName = result.Name;
-                    //rateType.Names = item.isim;
-                    //rateType.Buying = item.alis;
-                    //rateType.Selling = item.satis;
-                    //rateType.Changing = (Convert.ToDecimal(item.degisim) / 100);
-                    //rateTypeList.Add(rateType);
-                }
+                CoinModel coinModel = new CoinModel();
+                coinModel.Id = result.id;
+                coinModel.Name = result.name;
+                coinModel.Symbol = result.symbol;
+                coinModel.AddedDate = result.date_added;
+                coinModel.UpdateDate = result.last_updated;
+                coinModel.Price = result.quote.USD.price;               
+                coinModelLst.Add(coinModel);
             }
         }
     }
---
+++ Coin-Market-Example/Models/CoinModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Coin_Market_Example.Models
+{
+    public class CoinModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Symbol { get; set; }
+        public DateTime? AddedDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public double Price { get; set; }
+    }
+}
